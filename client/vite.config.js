e222bef6210ca3cb0bd0dd9b61472f85e198ff06
Afreen-707 <x423afreensayed@gmail.com>
2024-11-18 22:23:08 +0530

    added vite options

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,14 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      external: ['@headlessui/react']
-    }
-  },
-  optimizeDeps: {
-    include: ['@headlessui/react']
-  },
+
   server: {
     port: 3000,
     proxy: {
@@ -21,5 +14,4 @@ export default defineConfig({
       },
     },
   },
-
 });
